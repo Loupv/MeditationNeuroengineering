3cbc7b6f9005a1d631b2371d415e615fa269f815
loupv <loup.vuarnesson@gmail.com>
2022-10-18 16:42:39 +0200

    feat
    - post process configs in exvr
    - transitions in exvr
    - spatialized sound

--- ExVR/Assets/SceneFaderExVR.cs
+++ ExVR/Assets/SceneFaderExVR.cs
@@ -1,75 +1,79 @@
 // system
 using System;
-using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 
 // unity
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using System.Linq;
-
-namespace Ex{
-
-    public class SceneFaderExVR : BaseCompiledCSharpComponent{
-
-		public GameObject[] fadeScreens;
-		//UnityEngine.Rendering.Volume globalVolume;
-		public float fadeSpeed = 3.8f, waitingTimeBetweenScenes = 2f;
-		public string[] scenesArray;
-		public int currentScenesArrayID;
-		public bool hasFaded, isFading;
-
-		public List<Light> lights;
-		Light shelterTransitionLight;
-		public float[] lightsLastIntensity;
-		Material tmpSkybox;
-		float skyboxExposure, skyboxSunsize, skyboxConvergence, skyboxThickness;
+using UnityEngine;
 
 
-		public enum FadeDirection
-		{
-			In, //Alpha = 1
-			Out // Alpha = 0
-		}
+namespace Ex
+{
 
-		// ### Ex component flow functions, uncomment to use
-		// # main functions
-		// public override bool initialize() {return true;}
-		
-		public override void start_routine() {
-            PrepareNewSceneFadeIn();
-            StartCoroutine(Fade(FadeDirection.In));
+    public class SceneFaderExVR : BaseCompiledCSharpComponent
+    {
+
+        public GameObject[] fadeScreens;
+        //UnityEngine.Rendering.Volume globalVolume;
+        public float fadeSpeed = 3.8f, waitingTimeBetweenScenes = 2f;
+        public string[] scenesArray;
+        public int currentScenesArrayID;
+        public bool hasFaded, isFading;
+
+        public List<Light> lights;
+        Light shelterTransitionLight;
+        public float[] lightsLastIntensity;
+        Material tmpSkybox;
+        float skyboxExposure, skyboxSunsize, skyboxConvergence, skyboxThickness;
+
+
+        public enum FadeDirection
+        {
+            In, //Alpha = 1
+            Out // Alpha = 0
         }
 
-		// public override void start_routine() {}
-		public override void update() {
+        // ### Ex component flow functions, uncomment to use
+        // # main functions
+        // public override bool initialize() {return true;}
+        
 
-			if (UnityEngine.Input.GetKeyDown(KeyCode.Space) && !isFading)
-			{
-				isFading = true;
-				currentScenesArrayID += 1;
-				if (currentScenesArrayID >= scenesArray.Length) currentScenesArrayID = 0;
-				StartCoroutine(FadeOutAndLoadScene());
-			}
+        public override void start_routine()
+        {
+            StartCoroutine(PrepareNewSceneFadeIn());
+            
+        }
 
-		}
-		
+        public override void update()
+        {
+
+            if (UnityEngine.Input.GetKeyDown(KeyCode.Space) && !isFading)
+            {
+                isFading = true;
+                //currentScenesArrayID += 1;
+                //if (currentScenesArrayID >= scenesArray.Length) currentScenesArrayID = 0;
+                StartCoroutine(FadeOutAndLoadScene());
+            }
+        }
 
 
 
-		void PrepareNewSceneFadeIn()
-		{
-			//globalVolume = FindObjectOfType<UnityEngine.Rendering.Volume>();
-			//globalVolume.weight = 0;
 
-			// && obj.activeInHierarchy
-			fadeScreens = new GameObject[5];
-			fadeScreens = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => (obj.name == "ShelterScreen")).ToArray();
-			log_message("Count : "+fadeScreens.Length.ToString());
-			//fadeScreens = GameObject.FindGameObjectsWithTag("ShelterScreens");
+        private IEnumerator PrepareNewSceneFadeIn()
+        {
+            //globalVolume = FindObjectOfType<UnityEngine.Rendering.Volume>();
+            //globalVolume.weight = 0;
+            
+            yield return 0;
+             
+            fadeScreens = Resources.FindObjectsOfTypeAll<GameObject>().Where(obj => (obj.name == "ShelterScreen" && obj.activeInHierarchy)).ToArray();
+            log_message("Count : " + fadeScreens.Length.ToString());
+            foreach (GameObject screen in fadeScreens) log_message(screen.name);
 
-			/*try
+            //fadeScreens = GameObject.FindGameObjectsWithTag("ShelterScreens");
+
+            /*try
 			{
 				FindObjectOfType<OmniController>().myCamera = Camera.main.gameObject;
 			}
@@ -78,178 +82,183 @@ namespace Ex{
 				Debug.Log("No omni system or no main camera found");
 			}*/
 
-			CopyCurrentSkybox();
+            CopyCurrentSkybox();
 
-			GetLightsReferences();
-			ShutEveryLights();
-		}
-		
-		
+            GetLightsReferences();
+            ShutEveryLights();
 
-		private IEnumerator Fade(FadeDirection fadeDirection)
-		{
-			float alpha = (fadeDirection == FadeDirection.Out) ? 0 : 1;
-			float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 1 : 0;
+            yield return 0;
 
-			if (fadeDirection == FadeDirection.In)
-			{
-				log_message("fading in");
-				while (alpha >= fadeEndValue)
-				{
-					SetGOFadeAmount(ref alpha, fadeDirection);
-					log_message(alpha.ToString());
-					yield return null;
-				}
-				foreach (GameObject screen in fadeScreens) screen.SetActive(false);
-			}
-			else
-			{
-				log_message("fading out");
-				foreach (GameObject screen in fadeScreens) screen.SetActive(true);
-				while (alpha <= fadeEndValue)
-				{
-					SetGOFadeAmount(ref alpha, fadeDirection);
-					yield return null;
-				}
-			}
-
-			// fade cycle is now complete
-			if (fadeDirection == FadeDirection.In) isFading = false;
-		}
-
-		private void SetGOFadeAmount(ref float alpha, FadeDirection fadeDirection)
-		{
-			foreach (GameObject screen in fadeScreens)
-			{
-				Material mat = screen.GetComponent<MeshRenderer>().materials[0];
-				mat.SetColor("_Color", new Color(mat.color.r, mat.color.g, mat.color.b, alpha));
-			}
-
-			//shelterTransitionLight.intensity = alpha;
-
-			for (int i = 0; i < lights.Count; i++)
-			{
-				lights[i].intensity = lightsLastIntensity[i] * (1 - alpha);
-			}
-
-			//globalVolume.weight = 1 - alpha;
-
-			tmpSkybox.SetFloat("_SunSize", skyboxSunsize * (1 - alpha));
-			tmpSkybox.SetFloat("_SunSizeConvergence", skyboxConvergence * (1 - alpha));
-			tmpSkybox.SetFloat("_AtmosphereThickness", skyboxThickness * (1 - alpha));
-			tmpSkybox.SetFloat("_Exposure", skyboxExposure * (1 - alpha));
-
-			alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.In) ? -1 : 1);
-		}
-
-
-		public IEnumerator FadeOutAndLoadScene()
-		{
-			yield return Fade(FadeDirection.Out);
-			SceneManager.LoadScene(scenesArray[currentScenesArrayID]);
-
-			// remove already present dontdestroyonload object (atm shelter)
-
-			yield return 0;
-
-			//if (FindObjectsOfType<DontDestroyOnLoad>().Length > 1) Destroy(FindObjectsOfType<DontDestroyOnLoad>()[1].gameObject);
-
-			yield return 0; // wait for the frame to end and the objects to be destroyed
-
-			PrepareNewSceneFadeIn();
-
-			yield return new WaitForSeconds(waitingTimeBetweenScenes);
-			yield return Fade(FadeDirection.In);
-		}
+            StartCoroutine(Fade(FadeDirection.In));
+        }
 
 
 
+        private IEnumerator Fade(FadeDirection fadeDirection)
+        {
+            float alpha = (fadeDirection == FadeDirection.Out) ? 0 : 1;
+            float fadeEndValue = (fadeDirection == FadeDirection.Out) ? 1 : 0;
 
-		public void GetLightsReferences()
-		{
-			//shelterTransitionLight = GameObject.Find("ShelterTransitionLight").GetComponent<Light>();
+            if (fadeDirection == FadeDirection.In)
+            {
+                log_message("fading in");
+                while (alpha >= fadeEndValue)
+                {
+                    SetGOFadeAmount(ref alpha, fadeDirection);
+                    yield return null;
+                }
+                //foreach (GameObject screen in fadeScreens) screen.SetActive(false);
+            }
+            else
+            {
+                log_message("fading out");
+                //foreach (GameObject screen in fadeScreens) screen.SetActive(true);
+                while (alpha <= fadeEndValue)
+                {
+                    SetGOFadeAmount(ref alpha, fadeDirection);
+                    yield return null;
+                }
+            }
 
-			Light[] lightArray = FindObjectsOfType<Light>();
-			lights = new List<Light>();
+            // fade cycle is now complete
+            if (fadeDirection == FadeDirection.In) isFading = false;
+        }
 
-			foreach (Light light in lightArray)
-			{
-				//if (light.tag != "MeditationShelter") lights.Add(light);
-				if (light.gameObject.name != "ShelterTransitionLight") lights.Add(light);
-			}
+        private void SetGOFadeAmount(ref float alpha, FadeDirection fadeDirection)
+        {
+            foreach (GameObject screen in fadeScreens)
+            {
+                Material mat = screen.GetComponent<MeshRenderer>().materials[0];
+                mat.SetColor("_Color", new Color(mat.color.r, mat.color.g, mat.color.b, alpha));
+            }
 
-			lightsLastIntensity = new float[lights.Count];
+            shelterTransitionLight.intensity = alpha;
 
-			for (int i = 0; i < lights.Count; i++)
-			{
-				lightsLastIntensity[i] = lights[i].intensity;
-			}
-		}
+            for (int i = 0; i < lights.Count; i++)
+            {
+                lights[i].intensity = lightsLastIntensity[i] * (1 - alpha);
+            }
+
+            //globalVolume.weight = 1 - alpha;
+
+            tmpSkybox.SetFloat("_SunSize", skyboxSunsize * (1 - alpha));
+            tmpSkybox.SetFloat("_SunSizeConvergence", skyboxConvergence * (1 - alpha));
+            tmpSkybox.SetFloat("_AtmosphereThickness", skyboxThickness * (1 - alpha));
+            tmpSkybox.SetFloat("_Exposure", skyboxExposure * (1 - alpha));
+
+            alpha += Time.deltaTime * (1.0f / fadeSpeed) * ((fadeDirection == FadeDirection.In) ? -1 : 1);
+        }
 
 
-		void ShutEveryLights()
-		{
-			foreach (Light light in lights)
-			{
-				light.intensity = 0;
-			}
-		}
+        public IEnumerator FadeOutAndLoadScene()
+        {
+            yield return Fade(FadeDirection.Out);
+
+            next();
+            //SceneManager.LoadScene(scenesArray[currentScenesArrayID]);
+
+            // remove already present dontdestroyonload object (atm shelter)
+            /*
+            yield return 0;
+
+            //if (FindObjectsOfType<DontDestroyOnLoad>().Length > 1) Destroy(FindObjectsOfType<DontDestroyOnLoad>()[1].gameObject);
+
+            yield return 0; // wait for the frame to end and the objects to be destroyed
+
+            PrepareNewSceneFadeIn();
+
+            yield return new WaitForSeconds(waitingTimeBetweenScenes);
+            yield return Fade(FadeDirection.In);*/
+        }
 
 
-		void CopyCurrentSkybox()
-		{
-			// copy new skyboxparameters
-			skyboxSunsize = RenderSettings.skybox.GetFloat("_SunSize");
-			skyboxConvergence = RenderSettings.skybox.GetFloat("_SunSizeConvergence");
-			skyboxThickness = RenderSettings.skybox.GetFloat("_AtmosphereThickness");
-			skyboxExposure = RenderSettings.skybox.GetFloat("_Exposure");
 
-			//create a new one
-			tmpSkybox = new Material(RenderSettings.skybox.shader);
 
-			// set everything to 0
-			tmpSkybox.SetFloat("_Exposure", 0);
-			tmpSkybox.SetFloat("_SunSize", 0);
-			tmpSkybox.SetFloat("_SunSizeConvergence", 0);
-			tmpSkybox.SetFloat("_AtmosphereThickness", 0);
+        public void GetLightsReferences()
+        {
+            shelterTransitionLight = GameObject.Find("ShelterTransitionLight").GetComponent<Light>();
 
-			RenderSettings.skybox = tmpSkybox;
+            Light[] lightArray = FindObjectsOfType<Light>();
+            lights = new List<Light>();
 
-			//skyboxExposure = tmpSkybox.GetFloat("_Exposure");
-			//skyboxSunsize = tmpSkybox.GetFloat("_SunSize");
-			//skyboxConvergence = tmpSkybox.GetFloat("_SunSizeConvergence");
-			//skyboxThickness = tmpSkybox.GetFloat("_AtmosphereThickness");
-			//tmpSkybox.SetFloat("_SunSize", RenderSettings.skybox.GetFloat("_SunSize"));
-			//tmpSkybox.SetFloat("_SunSizeConvergence", RenderSettings.skybox.GetFloat("_SunSizeConvergence"));
-			//tmpSkybox.SetFloat("_AtmosphereThickness", RenderSettings.skybox.GetFloat("_AtmosphereThickness"));
-			//tmpSkybox.SetFloat("_Exposure", RenderSettings.skybox.GetFloat("_Exposure"));
+            foreach (Light light in lightArray)
+            {
+                //if (light.tag != "MeditationShelter") lights.Add(light);
+                if (light.gameObject.name != "ShelterTransitionLight") lights.Add(light);
+            }
 
-		}
+            lightsLastIntensity = new float[lights.Count];
 
-		// public override void stop_routine() {}
-		// public override void stop_experiment(){}
-		// public override void play(){}
-		// public override void pause(){}
-		// public override void set_update_state(bool doUpdate) { }        
-		// public override void set_visibility(bool visibility) { }
+            for (int i = 0; i < lights.Count; i++)
+            {
+                lightsLastIntensity[i] = lights[i].intensity;
+            }
+        }
 
-		// # for advanced users 
-		// public override void clean(){}
-		// public override void pre_update() {}
-		// public override void post_update() {}
-		// public override void update_parameter_from_gui(XML.Arg arg){}
-		// public override void update_from_current_config(){}
 
-		// # slots
-		// public override void slot1(object value){}
-		// public override void slot2(object value){}
-		// public override void slot3(object value){}
-		// public override void slot4(object value){}        
-		// public override void slot5(IdAny idValue){
-		// 	int id = idValue.id;
-		// 	object value = idValue.value;
-		// }  
-	}
+        void ShutEveryLights()
+        {
+            foreach (Light light in lights)
+            {
+                light.intensity = 0;
+            }
+        }
+
+
+        void CopyCurrentSkybox()
+        {
+            // copy new skyboxparameters
+            skyboxSunsize = RenderSettings.skybox.GetFloat("_SunSize");
+            skyboxConvergence = RenderSettings.skybox.GetFloat("_SunSizeConvergence");
+            skyboxThickness = RenderSettings.skybox.GetFloat("_AtmosphereThickness");
+            skyboxExposure = RenderSettings.skybox.GetFloat("_Exposure");
+
+            //create a new one
+            tmpSkybox = new Material(RenderSettings.skybox.shader);
+
+            // set everything to 0
+            tmpSkybox.SetFloat("_Exposure", 0);
+            tmpSkybox.SetFloat("_SunSize", 0);
+            tmpSkybox.SetFloat("_SunSizeConvergence", 0);
+            tmpSkybox.SetFloat("_AtmosphereThickness", 0);
+
+            RenderSettings.skybox = tmpSkybox;
+
+            //skyboxExposure = tmpSkybox.GetFloat("_Exposure");
+            //skyboxSunsize = tmpSkybox.GetFloat("_SunSize");
+            //skyboxConvergence = tmpSkybox.GetFloat("_SunSizeConvergence");
+            //skyboxThickness = tmpSkybox.GetFloat("_AtmosphereThickness");
+            //tmpSkybox.SetFloat("_SunSize", RenderSettings.skybox.GetFloat("_SunSize"));
+            //tmpSkybox.SetFloat("_SunSizeConvergence", RenderSettings.skybox.GetFloat("_SunSizeConvergence"));
+            //tmpSkybox.SetFloat("_AtmosphereThickness", RenderSettings.skybox.GetFloat("_AtmosphereThickness"));
+            //tmpSkybox.SetFloat("_Exposure", RenderSettings.skybox.GetFloat("_Exposure"));
+
+        }
+
+        // public override void stop_routine() {}
+        // public override void stop_experiment(){}
+        // public override void play(){}
+        // public override void pause(){}
+        // public override void set_update_state(bool doUpdate) { }        
+        // public override void set_visibility(bool visibility) { }
+
+        // # for advanced users 
+        // public override void clean(){}
+        // public override void pre_update() {}
+        // public override void post_update() {}
+        // public override void update_parameter_from_gui(XML.Arg arg){}
+        // public override void update_from_current_config(){}
+
+        // # slots
+        // public override void slot1(object value){}
+        // public override void slot2(object value){}
+        // public override void slot3(object value){}
+        // public override void slot4(object value){}        
+        // public override void slot5(IdAny idValue){
+        // 	int id = idValue.id;
+        // 	object value = idValue.value;
+        // }  
+    }
 }
 
 
