7d6c19048dc6a850c75b57349c6725b7a9cb1b71
Loupv <loup@vuarnesson.com>
2023-01-10 11:50:55 +0100

    integration sons

--- ExVR/Assets/SceneFaderExVR.cs
+++ ExVR/Assets/SceneFaderExVR.cs
@@ -73,8 +73,13 @@ namespace Ex
             guidanceClipNames = new Dictionary<string, string>();
             guidanceClipNames.Add("Lab_Intro","Guidance_Lab_Intro");
             guidanceClipNames.Add("Forest_Arrival","Guidance_Forest_Arrival");
-            guidanceClipNames.Add("Forest_MPS1", "Guidance_Forest_MPS1");
-            guidanceClipNames.Add("Lab_Return", "Guidance_Lab_WelcomeBack");
+            guidanceClipNames.Add("Forest_MPS1_PartA", "Guidance_Forest_MPS1_PartA");
+            guidanceClipNames.Add("Forest_MPS1_PartB", "Guidance_Forest_MPS1_PartB");
+            guidanceClipNames.Add("Forest_MPS2_PartA", "Guidance_Forest_MPS2_PartA");
+            guidanceClipNames.Add("Forest_MPS2_PartB", "Guidance_Forest_MPS2_PartB");
+            guidanceClipNames.Add("Forest_MPS3", "Guidance_Forest_MPS3");
+            guidanceClipNames.Add("Forest_Dissolution", "Guidance_Forest_Dissolution");
+            guidanceClipNames.Add("Lab_WelcomeBack", "Guidance_Lab_WelcomeBack");
             return guidanceClipNames;
         }
 
