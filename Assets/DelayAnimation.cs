71dcdc7e722d2bbc6a5fd626335bee154fca3fff
Loupv <loup.vuarnesson@gmail.com>
2024-10-24 16:50:29 +0200

    feat
    lotus, rocks, skysphere, lot of stuff for HsinPing study

---
+++ Assets/DelayAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayAnimation : MonoBehaviour
+{
+
+    float delay, speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        delay = Random.Range(0, 100);
+        speed = Random.Range(0.7f, 1);
+        GetComponent<Animator>().speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Animator>().speed = speed + Mathf.Sin(Time.time/5)*0.3f;
+    }
+}
